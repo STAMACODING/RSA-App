f454ed5bbd0782a1c680aaf824c2186ad4a707c6
BeneCodecademy <dareb.bene@gmail.com>
2020-05-30 15:39:57 +0200

    Add the logger class, which works like an interface.

---
+++ sourceCode/src/com/stamacoding/rsaApp/log/Logger.java
@@ -0,0 +1,40 @@
+package com.stamacoding.rsaApp.log;
+
+public class Logger {
+	/** only for test runs and unimportant information 
+	 * 
+	 * @param className input for class, that called the method
+	 * @param message input for message, typed by the calling class
+	 **/
+	public void test(String className, String message) {
+		
+	}
+	
+	/** only for unimportant information while program is running 
+	 * 
+	 * @param className input for class, that called the method
+	 * @param message input for message, typed by the calling class
+	 **/
+	public void debug(String className, String message) {
+		
+	}
+	
+	/** only for problems, that do not make the program stop 
+	 * 
+	 * @param className input for class, that called the method
+	 * @param message input for message, typed by the calling class
+	**/
+	public void warning(String className, String message) {
+		
+	}
+	
+	/** 
+	 * only for issues, that make the program stop 
+	 * 
+	 * @param className input for class, that called the method
+	 * @param message input for message, typed by the calling class
+	 **/
+	public void error(String className, String message) {
+	
+	}
+}
