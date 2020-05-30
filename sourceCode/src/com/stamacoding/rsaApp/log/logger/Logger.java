11ed7737b013ed5a35263e893757f67d684326ca
MrTK <dev.mrtk@gmail.com>
2020-05-30 19:54:38 +0200

    prepare the Logger for file saving
    The Logger-Class now has some options for file-saving. Not yet implemented.

--- sourceCode/src/com/stamacoding/rsaApp/log/logger/Logger.java
+++ sourceCode/src/com/stamacoding/rsaApp/log/logger/Logger.java
@@ -3,13 +3,18 @@ package com.stamacoding.rsaApp.log.logger;
 import com.stamacoding.rsaApp.log.debug.Debug;
 
 public class Logger {
+
+	public static outputType OutputType = outputType.ALL;
+
+	public static fileType FileType = fileType.ALL;
+
 	/** only for test runs and unimportant information 
 	 * 
 	 * @param className input for class, that called the method
 	 * @param message input for message, typed by the calling class
 	 **/
 	public static void test(String className, String message) {
-		console(className, message, logType.TEST);
+		log(className, message, logType.TEST);
 	}
 	
 	/** only for unimportant information while program is running 
@@ -18,7 +23,7 @@ public class Logger {
 	 * @param message input for message, typed by the calling class
 	 **/
 	public static void debug(String className, String message) {
-		console(className, message, logType.DEBUG);
+		log(className, message, logType.DEBUG);
 	}
 	
 	/** only for problems, that do not make the program stop 
@@ -27,7 +32,7 @@ public class Logger {
 	 * @param message input for message, typed by the calling class
 	**/
 	public static void warning(String className, String message) {
-		console(className, message, logType.WARNING);
+		log(className, message, logType.WARNING);
 	}
 	
 	/** 
@@ -37,19 +42,41 @@ public class Logger {
 	 * @param message input for message, typed by the calling class
 	 **/
 	public static void error(String className, String message) {
-		console(className, message, logType.ERROR);
+		log(className, message, logType.ERROR);
 	}
 	
+	static void log(String className, String message, logType Type)
+	{
+		if (OutputType != outputType.NONE)
+		{
+			String logMessage = "[" + Debug.Time() + "]" + /*"[" + status + "]" +*/ /*"[" + client/server + "]" +*/ "[" + className + "]" + "[" + Type.toString() + "]" + ": "+ "[" + message + "]";
+
+			if (OutputType == outputType.onlyConsole || OutputType == outputType.ALL) {
+				printToConsole(logMessage);
+			}
+	
+			if (OutputType == outputType.onlyFiles || OutputType == outputType.ALL) {
+				printToFile(logMessage);
+			}
+		}
+	}
+
 	/**
 	 * does all the console- (and maybe log-) entries, gets parameters from the four methods
-	 * 
-	 * @param className
-	 * @param message
-	 * @param Type describes, which method called -> test, debug, warning, error
+	 * @param logMessage final message that gets printed in the console
 	 */
-	public static void console(String className, String message, logType Type) {
-		String logMessage = "[" + Debug.Time() + "]" + /*"[" + status + "]" +*/ /*"[" + client/server + "]" +*/ "[" + className + "]" + "[" + Type.toString() + "]" + ": "+ "[" + message + "]";
+	static void printToConsole(String logMessage)
+	{
 		System.out.println(logMessage);
+	}
+
+	/**
+	 * does all the file entries, gets parameters from the four methods
+	 * @param logMessage final message that gets written to file(s)
+	 */
+	static void printToFile(String logMessage)
+	{
+
 	}
 }
 
@@ -59,4 +86,18 @@ enum logType
 	DEBUG,
 	WARNING,
 	ERROR
+}
+
+enum outputType
+{
+	NONE,
+	onlyConsole,
+	onlyFiles,
+	ALL
+}
+
+enum fileType
+{
+	ONE,
+	ALL
 }
