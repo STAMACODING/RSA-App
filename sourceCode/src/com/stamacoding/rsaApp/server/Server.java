47ef0a9dd79bcff8a57c4110bbb250aedf1a0824
harrydehix <harry.holly02@gmail.com>
2020-05-31 10:16:18 +0200

    Add log statements to Server.java
    This commit adds basic log statements to the Server.java file.

--- sourceCode/src/com/stamacoding/rsaApp/server/Server.java
+++ sourceCode/src/com/stamacoding/rsaApp/server/Server.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import com.stamacoding.rsaApp.log.logger.Logger;
+
 /**
  * Contains server functions to receive data from clients and forward it to other clients.
  */
@@ -16,6 +18,7 @@ public class Server {
 	 * method to start the server.
 	 */
 	public static void receive() {
+		Logger.debug(Server.class.getSimpleName(), "Starting server");
 		// Start new thread to not freeze gui
 		new Thread(new Runnable() {
 			
@@ -24,11 +27,11 @@ public class Server {
 				try {
 					// Creates server listening on port 455 (client should send data on this port)
 					ServerSocket server = new ServerSocket(455);
-					
+					Logger.debug(Server.class.getSimpleName(), "Server started successfully");
 					while(true) {
 						// Accept data package from client (port 455)
 						Socket receiver = server.accept();
-						
+						Logger.debug(Server.class.getSimpleName(), "Receiving new data package from a client");
 						DataInputStream inputStream = new DataInputStream(receiver.getInputStream());
 						
 						// Check if data package is not empty
@@ -38,16 +41,19 @@ public class Server {
 							byte[] dataPackage = new byte[length];
 						    // Read data package
 						    inputStream.readFully(dataPackage, 0, dataPackage.length);
+						    Logger.debug(Server.class.getSimpleName(), "Received new data package from a client");
 						    
 						    // Get meta Information
 							String targetIp = MetaUtils.getReceiving(dataPackage);
 							
 							// Forward data to specified client on port 456 (client should listen on this port)
+							Logger.debug(Server.class.getSimpleName(), "Trying to send data package to " + targetIp);
 							send(456, targetIp, dataPackage);
 						}
 					}
 					// server.close();
 				} catch (IOException e) {
+					Logger.error(Server.class.getSimpleName(), "Server couldn't start");
 					e.printStackTrace();
 				}
 			}
@@ -66,15 +72,16 @@ public class Server {
 			
 			DataOutputStream outputStream = new DataOutputStream(sending.getOutputStream());
 			
+			Logger.debug(Server.class.getSimpleName(), "Sending data package to " + ip);
 			// Set data length
 			outputStream.writeInt(dataPackage.length);
-			
 			// Send data
 			outputStream.write(dataPackage);
-			
+			Logger.debug(Server.class.getSimpleName(), "Sent data package successfully to " + ip);
 			// Close socket to enable new socket
 			sending.close();
 		} catch (IOException e) {
+			Logger.error(Server.class.getSimpleName(), "Failed to send data package to " + ip);
 			e.printStackTrace();
 		}
 	}
