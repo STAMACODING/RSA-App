4e96ee19442e13b6717bd70f7d70a054cbe449f4
harrydehix <harry.holly02@gmail.com>
2020-06-13 22:59:30 +0200

    Add prime number generation function

--- sourceCode/src/com/stamacoding/rsaApp/rsa/keyCreate/KeyUtils.java
+++ sourceCode/src/com/stamacoding/rsaApp/rsa/keyCreate/KeyUtils.java
@@ -1,10 +1,24 @@
 package com.stamacoding.rsaApp.rsa.keyCreate;
 
-public class KeyUtils {
+import java.util.ArrayList;
 
-	public static int primeNumb(int i, int p) {
-		// TODO Auto-generated method stub
-		return 0;
+public class KeyUtils {
+	
+	private static boolean isPrime(int i) {
+	    for(int j = 2; j<i; j++) {
+	    	if(i%j == 0)
+	    		return false;
+	    }
+	    return true;
+	}
+
+	public static int primeNumb(int max, int excludedNumber) {
+		ArrayList<Integer> primes = new ArrayList<>();
+		for(int j = 2; j <= max; j++) {
+		    if(isPrime(j) && j!=excludedNumber) primes.add(j);
+		}
+		int randomIndex = (int) (Math.random()*primes.size());
+		return primes.get(randomIndex);
 	}
 
 	public static int modularInverse(int phi, int e) {
@@ -12,8 +26,7 @@ public class KeyUtils {
 		return 0;
 	}
 
-	public static int primeNumb(int i) {
-		// TODO Auto-generated method stub
-		return 0;
+	public static int primeNumb(int max) {
+		return primeNumb(max, -1);
 	}
 }
