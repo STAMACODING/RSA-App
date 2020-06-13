345f63b3e195c1a2d7be2f01f6cca94b983ebb64
BeneCodecademy <dareb.bene@gmail.com>
2020-06-13 23:27:09 +0200

    Add documentation for KeyUtils.java and Key.java

--- sourceCode/src/com/stamacoding/rsaApp/rsa/keyCreate/Key.java
+++ sourceCode/src/com/stamacoding/rsaApp/rsa/keyCreate/Key.java
@@ -1,7 +1,15 @@
 package com.stamacoding.rsaApp.rsa.keyCreate;
-
+/**
+ * object represents a key, depending on the incoming values for exponent and module
+ */
 public class Key {
+	/**
+	 * holds the exponent in a modulo operation
+	 */
 	private int exp = 0;
+	/**
+	 * holds the modulo number in a modulo operation
+	 */
 	private int mod = 0;
 	
 	public Key(int exp, int mod) {
--- sourceCode/src/com/stamacoding/rsaApp/rsa/keyCreate/KeyUtils.java
+++ sourceCode/src/com/stamacoding/rsaApp/rsa/keyCreate/KeyUtils.java
@@ -2,6 +2,10 @@ package com.stamacoding.rsaApp.rsa.keyCreate;
 
 import java.util.ArrayList;
 
+/**
+ * holds functions like creating a prime number (in a certain area) or returning the modular inverse
+ */
+
 public class KeyUtils {
 	
 	private static boolean isPrime(int i) {
@@ -12,6 +16,12 @@ public class KeyUtils {
 	    return true;
 	}
 
+	/**
+	 * creates a prime number in a certain area
+	 * @param max maximum value of the area
+	 * @param excludedNumber all numbers that will not be accepted as possible prime numbers
+	 * @return
+	 */
 	public static int primeNumb(int max, int excludedNumber) {
 		ArrayList<Integer> primes = new ArrayList<>();
 		for(int j = 2; j <= max; j++) {
@@ -21,6 +31,12 @@ public class KeyUtils {
 		return primes.get(randomIndex);
 	}
 
+	/**
+	 * returns the modular inverse in a modulo operation
+	 * @param e number which stands in the base
+	 * @param m number which stands in the module
+	 * @return
+	 */
 	public static int modularInverse(int e, int m) {
 		boolean gefunden = false;
 		int d = 1;
@@ -37,6 +53,11 @@ public class KeyUtils {
 		return d;
 	}
 
+	/**
+	 * creates a prime number in a certain area, which reaches from 2 to a maximum value
+	 * @param max maximum value of the area
+	 * @return
+	 */
 	public static int primeNumb(int max) {
 		return primeNumb(max, -1);
 	}
