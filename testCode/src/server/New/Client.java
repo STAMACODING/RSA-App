79d0f83ca8b4b4a21d8252257ccf54ed5a178312
Hendrik Wirthwein <hendrik.wirthwein@gmail.com>
2020-06-29 21:03:32 +0200

    updated Client and Server classes

--- testCode/src/server/New/Client.java
+++ testCode/src/server/New/Client.java
@@ -3,12 +3,14 @@ package server.New;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import com.stamacoding.rsaApp.log.logger.Logger;
+
 public class Client {
 
 	Socket sock;
 	PrintWriter writer;
 	
-	
+	public static String clientName;
 	public static String CLIENT_IP;
 	public static int CLIENT_PORT;
 	
@@ -16,7 +18,8 @@ public class Client {
 	public final static String SERVER_IP = "127.0.0.1";
 	public final static int SERVER_PORT = 455;
 	
-	public Client(String ip, int port) {
+	public Client(String name, String ip, int port) {
+		this.clientName = name;
 		this.CLIENT_IP = ip;
 		this.CLIENT_PORT = port;
 	}
@@ -24,16 +27,20 @@ public class Client {
 	public static void run() {
 		
 		Runnable receive = new server.New.ReceiveRunnable(SERVER_PORT, false);
+		Logger.debug(Server.class.getSimpleName(), "ReceiveRunnable of Client:(" + clientName + ") initiated as: receive");
 		Runnable send = new server.New.SendRunnable(CLIENT_PORT);
+		Logger.debug(Server.class.getSimpleName(), "SendRunnable of Client:(" + clientName + ") initiated as: send");
 		
 		Thread receiveThread = new Thread(receive);
 		Thread sendThread = new Thread(send);
 		
 		receiveThread.start();
 		receiveThread.setName("receiveThread");
+		Logger.debug(Server.class.getSimpleName(), "receiveThrad of Client:(" + clientName + ") started") ;
 		
 		sendThread.start();	
 		sendThread.setName("sendThread");
+		Logger.debug(Server.class.getSimpleName(), "sendThrad of Client:(" + clientName + ") started") ;
 	}
 }
 
--- testCode/src/server/New/Server.java
+++ testCode/src/server/New/Server.java
@@ -1,27 +1,42 @@
 package server.New;
 
 import java.io.PrintWriter;
+
 import java.net.Socket;
+
+import com.stamacoding.rsaApp.log.logger.Logger;
 
 public class Server {
 	
 	Socket sock;
 	PrintWriter writer;
+	
+	public static String serverName;
 	public final static String SERVER_IP = "127.0.0.1";
 	public final static int SERVER_PORT = 455;
+	
+	public Server(String name) {
+		this.serverName = name;
+	}
 
 	public static void run() {
 		
 		Runnable receive = new server.New.ReceiveRunnable(SERVER_PORT, true);
+		Logger.debug(Server.class.getSimpleName(), "ReceiveRunnable of Server:(" + serverName + ") initiated as: receive");
 		Runnable send = new server.New.SendRunnable(SERVER_PORT);
+		Logger.debug(Server.class.getSimpleName(), "SendRunnable of Server:(" + serverName + ") initiated as: send");
 		
 		Thread receiveThread = new Thread(receive);
 		Thread sendThread = new Thread(send);
 		
 		receiveThread.start();
 		receiveThread.setName("receiveThread");
+		Logger.debug(Server.class.getSimpleName(), "receiveThrad of Server:(" + serverName + ") started") ;
+		receiveThread.setName("receiveThread");
 		
 		sendThread.start();	
+		sendThread.setName("sendThread");
+		Logger.debug(Server.class.getSimpleName(), "sendThrad of Server:(" + serverName + ") started");
 		sendThread.setName("sendThread");
 	}
 	
