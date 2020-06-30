d74c2baef75145f3f5c13de78da6e37446720945
harrydehix <harry.holly02@gmail.com>
2020-06-30 19:54:17 +0200

    Update TestServer

--- testCode/src/server/test/TestServer.java
+++ testCode/src/server/test/TestServer.java
@@ -4,20 +4,25 @@ import java.util.Scanner;
 
 import server.Client;
 import server.Server;
+import server.Utils;
 
 public class TestServer {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("------------------------------------------------------------");
-		System.out.println("Client receive port: ");
+		System.out.println("RSA-App Test Client BETA");
+		System.out.println("------------------------------------------------------------");
+		
+		System.out.print("Client receive port: ");
 		Client.RECEIVE_PORT = s.nextInt();
-		System.out.println("Server receive port: ");
+		
+		System.out.print("Server receive port: ");
 		Server.RECEIVE_PORT = s.nextInt();
-		System.out.println("Server ip: ");
-		Server.SERVER_IP = s.next();
+		
 		s.close();
 		System.out.println("------------------------------------------------------------");
 		
+		Server.SERVER_IP = Utils.Ip.getIpAdress();
 		Server.run();
 	}
 }
