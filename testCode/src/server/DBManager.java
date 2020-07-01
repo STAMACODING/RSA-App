3e550855ba3b8ce6d5fdff0cc458a5011947ca7e
Hendrik Wirthwein <hendrik.wirthwein@gmail.com>
2020-07-01 16:14:27 +0200

    updated DBManager

--- testCode/src/server/DBManager.java
+++ testCode/src/server/DBManager.java
@@ -1,12 +1,28 @@
 package server;
 
+import com.stamacoding.rsaApp.log.logger.Logger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBManager implements MessageDBInterface{
 
-	private final String url = "";
+	private final String url = "jdbc:sqlite:ChatHistory.db";
 	private final String userName = "root";
 	private final String password = "root";
 
 	public  void setUpConnection() throws Exception{
+		
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection c = DriverManager.getConnection(url, userName, password);
+			Logger.debug(Server.class.getSimpleName(), "Connectin to ChatHistory DB succesful");
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			Logger.debug(Server.class.getSimpleName(), "Connection to ChatHistory DB failed");
+		}
 		
 	}
 	
