bad05e1338da19ff186247f1c522f670f66c2b15
MrTK <dev.mrtk@gmail.com>
2020-07-03 15:30:46 +0200

    make syntax great again! (Debug.java)

--- sourceCode/src/com/stamacoding/rsaApp/log/debug/Debug.java
+++ sourceCode/src/com/stamacoding/rsaApp/log/debug/Debug.java
@@ -3,9 +3,8 @@ package com.stamacoding.rsaApp.log.debug;
 import java.text.DecimalFormat;
 import java.time.OffsetDateTime;
 
-import com.stamacoding.rsaApp.log.filesystem.Filesystem;
+//import com.stamacoding.rsaApp.log.filesystem.Filesystem;
 import com.stamacoding.rsaApp.log.logger.Logger;
-import com.stamacoding.rsaApp.log.filesystem.Filesystem;
 
 public class Debug
 {
@@ -27,24 +26,43 @@ public class Debug
      * 
      * @return current time as formatted time (yyyy-mm-ddThh:mm:ss:mmmm+zz:zz) (date-time-time zone)
      */
-    public static String TimeFormat() {
-    	String time = Time().toString();
-    	double timeDouble = 0;
-    	int timeLen = time.length();
-    	DecimalFormat df = new DecimalFormat("##.###"); //formatiert Zeit auf 4 Nachkommastellen(letzte ist immer 0)
-    	StringBuilder saveTime = new StringBuilder(time);
-    	StringBuilder timeB = new StringBuilder(time); //Zeit wird in StringBuilder gegeben
-    	for(int i = 0; i < 17; i++) { //erste 17 Stellen (Datum, Stunden, Minuten) werden gelöscht
+    public static String TimeFormat()
+    {
+        //#region Variables
+
+        String time = Time().toString();
+        
+        double timeDouble = 0;
+        
+        int timeLen = time.length();
+        
+        DecimalFormat df = new DecimalFormat("##.###"); //formatiert Zeit auf 4 Nachkommastellen(letzte ist immer 0)
+        
+        StringBuilder saveTime = new StringBuilder(time);
+        
+        StringBuilder timeB = new StringBuilder(time); //Zeit wird in StringBuilder gegeben
+        
+        //#endregion
+
+        //#region  Time
+
+        for(int i = 0; i < 17; i++) { //erste 17 Stellen (Datum, Stunden, Minuten) werden geloescht
     		timeB.deleteCharAt(0);
-    	}
-    	for(int i = 0; i <= 5; i++) { //letzte 5 Stellen (Zeitzone) werden gelöscht
+        }
+        
+    	for(int i = 0; i <= 5; i++) { //letzte 5 Stellen (Zeitzone) werden geloescht
     		timeB.deleteCharAt(timeB.length()-1);
-    	}
-    	time = timeB.toString();
-    	timeDouble = Double.parseDouble(time);
-    	
-    	String ret = saveTime.replace(17, timeLen-7, df.format(timeDouble)).toString(); //alte Sekunden werden durch formatierte Sekunden ersetzt
-    	return ret; //DecimalFormat formatiert Double zu String mit 3 Nachkommastellen
+        }
+        
+        time = timeB.toString();
+        
+        timeDouble = Double.parseDouble(time);
+        
+        String ret = saveTime.replace(17, timeLen - 7, df.format(timeDouble)).toString(); //alte Sekunden werden durch formatierte Sekunden ersetzt
+        
+        return ret; //DecimalFormat formatiert Double zu String mit 3 Nachkommastellen
+
+        //#endregion
     }
 
     /**
