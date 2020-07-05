804ecb464949354486b0d31a15f3e999f6cb4024
Henri Hollmann <harry.holly02@gmail.com>
2020-07-05 20:08:44 +0200

    Add key pair speed and correctness test

---
+++ testCode/src/rsa/KeyPairValidationer.java
@@ -0,0 +1,36 @@
+package rsa;
+
+import com.stamacoding.rsaApp.rsa.endecrypt.EnDecrypt;
+import com.stamacoding.rsaApp.rsa.keyCreate.KeyPair;
+
+public class KeyPairValidationer {
+	public static boolean validate(KeyPair kp) {
+		int messageMaximumLength = 50;
+		int messageMinimumLength = 25;
+		int randomSize = (int) (Math.random()*(messageMaximumLength-messageMinimumLength) + messageMinimumLength);
+		
+		long[] testArray = new long[randomSize];
+		for(int j = 0; j<randomSize; j++) testArray[j] = (long) (Math.random()*1000L);
+		
+		long[] newArray = EnDecrypt.decrypt(EnDecrypt.encrypt(testArray, kp.getPublicKey()), kp.getPrivateKey());
+		for(int j = 0; j<randomSize; j++) {
+			if(newArray[j] != testArray[j]) return false;
+		}
+		return true;
+	}
+	
+	public static void validateRandomKeyPairs(int testKeyPairCount) {
+		int errors = 0;
+		for(int i=0; i<testKeyPairCount; i++) {
+			KeyPair kp = new KeyPair();
+			if(!validate(kp)) errors++;
+		}
+		System.out.println("-------------------------------------------------------------");
+		System.out.println("Key pair vailidation test");
+		System.out.println("-------------------------------------------------------------");
+		System.out.printf("%-30s %s\n", "Key pairs tested:", testKeyPairCount);
+		System.out.printf("%-30s %s\n", "Valid key pairs:", testKeyPairCount-errors);
+		System.out.printf("%-30s %s\n", "Invalid key pairs:", errors);
+		System.out.println("-------------------------------------------------------------");
+	}
+}
