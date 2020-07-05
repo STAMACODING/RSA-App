b261a2a052225ecc0aad13a1cf514908c8cf7837
MrTK <dev.mrtk@gmail.com>
2020-07-05 18:27:43 +0200

    Fixes the complicated way of getting the current time
    The Complicated TimeFormat() gets replaced by GetTime().
    Closes #23
    
    Co-Authored-By: Henri Hollmann <harrydehix@users.noreply.github.com>

--- sourceCode/src/com/stamacoding/rsaApp/log/debug/Debug.java
+++ sourceCode/src/com/stamacoding/rsaApp/log/debug/Debug.java
@@ -1,7 +1,8 @@
 package com.stamacoding.rsaApp.log.debug;
 
-import java.text.DecimalFormat;
-import java.time.OffsetDateTime;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import com.stamacoding.rsaApp.log.logger.Logger;
 
@@ -12,66 +13,28 @@ public class Debug
     public static State State;
 
     /**
-     * Returns the current time in the ISO-8601 system.
-     * @return current time
-     */
-    public static OffsetDateTime Time()
-    {
-    	return OffsetDateTime.now();
-        //return String.format("%.2f", OffsetDateTime.now());//Zeit wird auf 3 Nachkommastellen gerundet
-    };
-    
-    /**
-     * 
-     * @return current time as formatted time (yyyy-mm-ddThh:mm:ss:mmmm+zz:zz) (date-time-time zone)
-     */
-    public static String TimeFormat()
-    {
-        //#region Variables
-
-        String time = Time().toString();
-        
-        double timeDouble = 0;
-        
-        int timeLen = time.length();
-        
-        DecimalFormat df = new DecimalFormat("##.###"); //formatiert Zeit auf 4 Nachkommastellen(letzte ist immer 0)
-        
-        StringBuilder saveTime = new StringBuilder(time);
-        
-        StringBuilder timeB = new StringBuilder(time); //Zeit wird in StringBuilder gegeben
-        
-        //#endregion
-
-        //#region  Time
-
-        for(int i = 0; i < 17; i++) { //erste 17 Stellen (Datum, Stunden, Minuten) werden geloescht
-    		timeB.deleteCharAt(0);
-        }
-        
-    	for(int i = 0; i <= 5; i++) { //letzte 5 Stellen (Zeitzone) werden geloescht
-    		timeB.deleteCharAt(timeB.length()-1);
-        }
-        
-        time = timeB.toString();
-        
-        timeDouble = Double.parseDouble(time);
-        
-        String ret = saveTime.replace(17, timeLen - 7, df.format(timeDouble)).toString(); //alte Sekunden werden durch formatierte Sekunden ersetzt
-        
-        return ret; //DecimalFormat formatiert Double zu String mit 3 Nachkommastellen
-
-        //#endregion
-    }
-
-    /**
      * Only for testing purposes
      * @param args
      */
-    public static void main(String[] args) {
-        Logger.debug(Debug.class.getSimpleName(), "Wer das liest hat Probleme");
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 10; i++) {
+            //Logger.debug(Debug.class.getSimpleName(), "Wer das liest hat Probleme");
+            Logger.debug(Debug.class.getSimpleName(), GetTime());
+            Thread.sleep(1000);
+        }
 
         //Filesystem.createFile();
+    }
+
+    /**
+     * Returns current Time [yyyy'-'MM'-'dd'T'HH':'mm':'ss','SSZ]
+     * @return current Time
+     */
+    public static String GetTime()
+    {
+        DateFormat dfmt = new SimpleDateFormat("yyyy'-'MM'-'dd'T'HH':'mm':'ss','SSZ");
+
+        return dfmt.format(new Date());
     }
 }
 
