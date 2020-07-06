dc9dfed4a56b4103db0facdad383437c60c9b740
MrTK <dev.mrtk@gmail.com>
2020-07-06 18:43:52 +0200

    Update Filesystem to fit for more stuff
    Added some checks and functions for creating a file and append lines to it.
    Closes #22

--- sourceCode/src/com/stamacoding/rsaApp/log/filesystem/Filesystem.java
+++ sourceCode/src/com/stamacoding/rsaApp/log/filesystem/Filesystem.java
@@ -1,34 +1,66 @@
 package com.stamacoding.rsaApp.log.filesystem;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class Filesystem
 {
-    public static void createFile(){
+    public static void createFile(String filePath, String fileName, FileEnding fileEnding)
+    {
+        createFile(filePath, fileName, fileEnding, null);
+    }
+
+    public static void createFile(String filePath, String fileName, FileEnding fileEnding, String firstMessage)
+    {
         try {
-            String testFile = "TestFile.txt";
+            String fileFull = getFullFileName(filePath, fileName, fileEnding);
 
-            String testString = "Hello World";
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileFull));
 
-            BufferedWriter writer = new BufferedWriter(new FileWriter(testFile));
-
-            writer.write(testString);
+            if (firstMessage != null)
+            {
+                writer.write(firstMessage);
+            }
 
             writer.close();
+
         } catch (Exception e) {
 
         }
-    }   
+    }
 
     static void writeToFile()
     {
 
     }
 
-    static void appendToFile()
+    public static void appendToFile(String filePath, String fileName, FileEnding fileEnding, String Message)
     {
+        String fileFull = getFullFileName(filePath, fileName, fileEnding);
 
+        //defines the FILEWRITER
+        FileWriter fileWriter = null;
+
+        if (checkFile(fileFull) == false)
+        {
+            createFile(filePath, fileName, fileEnding, "firstMessage");
+        }
+        
+		// try-catch just to make sure, all is in place
+        try
+        {
+            fileWriter = new FileWriter(fileFull, true); //true sets property of appending text to file and not overwriting it
+            
+            fileWriter.write("\n" + Message); //writes the log-message
+            
+			fileWriter.close(); //saves the file
+        } catch (IOException e)
+        {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
         
     static void deleteFile()
@@ -39,5 +71,24 @@ public class Filesystem
     static void deleteLines()
     {
         
+    }
+
+    static String getFullFileName(String filePath, String fileName, FileEnding fileEnding)
+    {
+        return filePath + "/" + fileName + "." + fileEnding.toString();
+    }
+
+    static boolean checkFile(String fullFileName)
+    {
+        return new File(fullFileName).exists();
+    }
+
+    public enum FileEnding
+    {
+        none,
+        log,
+        txt,
+        xml,
+        json
     }
 }
