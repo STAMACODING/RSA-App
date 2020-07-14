8592aafe1f3b551245b9a05a1173d6a9807eee1c
saltyMrTK <Dev.MrTK@gmail.com>
2020-07-14 18:24:56 +0200

    Add documentation to the Logger
    Henri was mean to me so I had to include silly jokes. I hope no one feels offended by them. Have a nice day!

--- sourceCode/src/com/stamacoding/rsaApp/log/logger/Logger.java
+++ sourceCode/src/com/stamacoding/rsaApp/log/logger/Logger.java
@@ -1,10 +1,19 @@
 package com.stamacoding.rsaApp.log.logger;
 
 import java.io.File;
-
 import com.stamacoding.rsaApp.log.debug.Debug;
 import com.stamacoding.rsaApp.log.filesystem.Filesystem;
 
+/**
+ * <p> This class handles everything that is related to sending messages in the console or saving those information in log files. </p>
+ * <p> There are 4 types of log messages: </p>
+ * <ol>
+ * 		<li>Test: {@link Logger#test(String, String)}</li>
+ * 		<li>Debug: {@link Logger#debug(String, String)}</li>
+ * 		<li>Warning: {@link Logger#warning(String, String)}</li>
+ * 		<li>Error: {@link Logger#error(String, String)}</li>
+ * </ol>
+ */
 public class Logger {
 	
 	static{
@@ -13,20 +22,42 @@ public class Logger {
 		f.mkdirs();
 	}
 
+	/**
+	 * Sets the Output Type.
+	 */
 	public static outputType OutputType = outputType.ALL;
 
+	/**
+	 * [WIP]
+	 * Sets the Type of files that get written.
+	 */
 	public static fileType FileType = fileType.ALL;
 
+	/**
+	 * First lines that get written to every log file.
+	 * Can be as long as you want to but you should look for the overflow settings to avoid conflicts.
+	 */
 	static String[] startingMessage = new String[] {"Thiszfghjk", "is", "a", "test"};
 
+	/**
+	 * Path to the folder that contains all log files.
+	 * TODO this should be relative to avoid errors
+	 */
 	static String logPath = "logs";
 
+	/**
+	 * Name of the main log file.
+	 */
 	static String fileName = "Main";
 
+	/**
+	 * The Maximum of Lines that one log file can hold.
+	 * TODO this should be independent of the startingMessage
+	 */
 	static int maxLinesCount = 50;
 
-	/** only for test runs and unimportant information 
-	 * 
+	/** 
+	 * only for test runs and unimportant information
 	 * @param className input for class, that called the method
 	 * @param message input for message, typed by the calling class
 	 **/
@@ -52,7 +83,7 @@ public class Logger {
 		log(className, message, logType.WARNING);
 	}
 	
-	/** 
+	/**
 	 * only for issues, that make the program stop 
 	 * 
 	 * @param className input for class, that called the method
@@ -63,10 +94,10 @@ public class Logger {
 	}
 	
 	/**
-	 * 
+	 * Handles log messages.
 	 * @param className Cannot be longer than 20 Characters
-	 * @param message
-	 * @param Type
+	 * @param message Message from the function caller
+	 * @param Type Type of message
 	 */
 	static void log(String className, String message, logType Type)
 	{
@@ -85,13 +116,16 @@ public class Logger {
 	}
 
 	/**
-	 * does all the console- (and maybe log-) entries, gets parameters from the four methods
+	 * does all the console entries, gets parameters from the four methods
 	 * @param logMessage final message that gets printed in the console
 	 */
 	static void printToConsole(String logMessage) {
 		System.out.println(logMessage);
 	}
 
+	/**
+	 * Helps to optimise the function.
+	 */
 	static int startCounter = 0;
 
 	/**
@@ -100,6 +134,7 @@ public class Logger {
 	 */
 	static void printToFile(String logMessage)
 	{
+		//Only checks for an existing file if it is the first run of the logger.
 		if (startCounter == 0) {
 			if (!Filesystem.checkFile(Filesystem.getFullFileName(logPath, fileName, Filesystem.FileEnding.log)))
 				Filesystem.createFile(logPath, fileName, Filesystem.FileEnding.log, startingMessage);
@@ -110,22 +145,56 @@ public class Logger {
 	}
 }
 
+/**
+ * Type of the log message.
+ */
 enum logType
 {
+	/**
+	 * For test runs and unimportant information.
+	 */
 	TEST,
+	/**
+	 * For unimportant information while program is running.
+	 */
 	DEBUG,
+	/**
+	 * For problems, that do not make the program stop.
+	 */
 	WARNING,
+	/**
+	 * For issues, that make the program stop.
+	 */
 	ERROR
 }
 
+/**
+ * Type of Visibility of log messages.
+ */
 enum outputType
 {
+	/**
+	 * Log messages be gone!
+	 */
 	NONE,
+	/**
+	 * Only the console is strong enough to carry the messages out of the application!
+	 */
 	onlyConsole,
+	/**
+	 * Console is old - let us use files but only files!
+	 */
 	onlyFiles,
+	/**
+	 * Perfectly balanced (Console & Files), as all things should be.
+	 */
 	ALL
 }
 
+/**
+ * [WIP]
+ * Type of files written by Logger.
+ */
 enum fileType
 {
 	ONE,
